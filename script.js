a50bf1d0667a15bb1ab6ae4291f988d565cda3c6
Marjoy1516 <marcinho2113@gmail.com>
2024-03-12 14:16:28 -0300

    Menu da pagina inicial finalizado

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
   }
+
+function toggleMenuEscudo() {
+  const menuEscudo = document.getElementById('menuEscudo');
+  menuEscudo.classList.toggle('activeEscudo');
+}
